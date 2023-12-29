329bfd24b20489aa92f45928ebcbe676f4cdbd8d
Rafael Mendes <rafael1jmendes@icloud.com>
2023-12-29 03:26:26 -0300

    11º adc keyframes no switch button e ajustando dimensao

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function toggleMode(){
    const html = document.documentElement
-   
-    // if(html.classList.contains ('light')){
-   // html.classList.remove('light')
-  // } else{ html.classList.add('light')
-
-  // } 
-
    html.classList.toggle('light')
+   
+   //pegar a tag img
+   const img = document.querySelector('#profile img')
+   
+   
+   //substituir a image
+   if(html.classList.contains ('light')){
+      
+      // se tiver light mode adc image light
+      img.setAttribute('src', './links/assets/avatar-light.png')
+ } else { 
+    //se tiver dark manter image normal
+img.setAttribute('src', "./links/assets/avatar.png")
+
+ }
 }
